574aa27915f23d1dec9216755409139216a0d136
fangzhiheng <fangzhiheng@corp.netease.com>
2022-11-09 20:29:10 +0800

    finish hashmaps

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -11,7 +11,6 @@
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -36,7 +35,10 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     for fruit in fruit_kinds {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
-        // present!
+        // present
+        if !basket.contains_key(&fruit) {  // why ref?
+            basket.insert(fruit, 2);
+        }
     }
 }
 
